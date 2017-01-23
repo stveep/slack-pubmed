8ca837bf99b304fad184d8087398c1552c9b8442
stveep <spettitt@gmail.com>
2017-01-23 15:20:39 +0000

    Reactivate; allow setting a maximum number of results

--- pubmed_slack.py
+++ pubmed_slack.py
@@ -1,11 +1,14 @@
 import pubmed
 from urllib2 import Request, urlopen
-search_terms = ["PARP1", "synthetic lethality"]
-slack_webhook = "https://hooks.slack.com/services/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"
+import os
+search_terms = ["Breast cancer"]
+
+slack_webhook = os.environ.get("SLACK_WEBHOOK_URL")
+max_results = 1
 
 for term in search_terms:
     search = pubmed.SearchResult(term)
-    for record in search.records():
+    for record in search.records()[0:max_results]:
         print record.slack_payload()
         request = Request(slack_webhook, record.slack_payload())
-        # urlopen(request).read().decode()
+        urlopen(request).read().decode()
